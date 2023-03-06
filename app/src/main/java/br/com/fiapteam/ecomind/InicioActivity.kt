22d9e5b439e055990a849a47ed31ca09a5538904
caio myra pereira <caiomyrapereira@hotmail.com>
2023-03-06 20:07:52 -0300

    add page inicio

---
+++ app/src/main/java/br/com/fiapteam/ecomind/InicioActivity.kt
@@ -0,0 +1,14 @@
+package br.com.fiapteam.ecomind
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+
+class InicioActivity : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_inicio)
+        }
+}
